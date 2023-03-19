0109036f0f267a7b463005b90c61c4f77f0f9368
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2023-03-20 02:50:07 +0300

    Single Responsibility Prenciple

---
+++ SingleResponsibilityPrinciple/IdealCode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple_IdealCode
+{
+    class ArithmeticOperations
+    {
+        //SRP sınıflarımızın veyahut methodlarımızın iyi tanımlanmış tek bir sorumluluğu olması gerektiğini anlatmaktadır. 
+        //Bu sınıf sadece aritmetik işlemleri gerçekleştirme sorumluluğu almıştır.
+
+        //Toplama ve çıkarma işlemi ayrı fonksiyonlar içerisinde yazılarak SRP'ye uyulmuştur.
+        public void Addition(int number1, int number2)
+        {
+            int additionResult = number1 + number2;
+            Console.WriteLine($"Toplama işlemi sonucu : {additionResult}");
+        }
+
+        public void subtraction(int number1, int number2)
+        {
+            int subtractionResult = number1 - number2;
+            Console.WriteLine($"Çıkarma işlemi sonucu : {subtractionResult}");
+        }
+    }
+
+    class StringOperations
+    {
+        //Bu sınıf sadece metin işlemleri gerçekleştirme sorumluluğu almıştır.
+        public void StringConvertToLower(string text)
+        {
+            Console.WriteLine("Metnin küçük harf dönüşmüş hali : ", text.ToLower());
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/NotİdealCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple_NotİdealCode
+{
+    class ArithmeticOperations
+    {
+        // SRP sınıflarımızın veyahut methodlarımızın iyi tanımlanmış tek bir sorumluluğu olması gerektiğini söyler. 
+        // Bir sınıf ne kadar fazla sorumluluk alırsa, o kadar fazla değişime uğramak zorunda kalır!
+
+        //Bu method toplama ve çıkarma işlemi olmak üzere iki farklı sorumluluk üstlenmiştir.
+        //Herhangi bir sorumluluktaki değişiklik diğer sorumluluğu bozabilir - engelleyebilir.
+        public void AdditionAndSubtraction(int number1,int number2)
+        {
+            int additionResult = number1 + number2;
+            Console.WriteLine($"Toplama işlemi sonucu : {additionResult}");
+
+            int subtractionResult = number1 - number2;
+            Console.WriteLine($"Çıkarma işlemi sonucu : {subtractionResult}");
+        }
+
+        
+        //Bu method bu sınıfa aritmetik işlemlerin dışında string işlem sorumluluğu da yüklemiştir.
+        public void StringConvertToLower(string text)
+        {
+            Console.WriteLine("Metnin küçük harf dönüşmüş hali : ", text.ToLower());
+        }
+    }
+}
